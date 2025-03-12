672f6630e2a6fe0e08ecc183b301eba2a3e6f331
heabeounMKTO <heabeoun69zin@gmail.com>
2025-03-12 15:45:22 +0700

    init

---
+++ onnx_utils.h
@@ -0,0 +1,125 @@
+// header-only library for loading generic onnx models.
+#ifndef ONNX_UTILS_H
+#define ONNX_UTILS_H
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <onnxruntime_c_api.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+  const OrtApi *api;
+  OrtEnv *env;
+  OrtSession *session;
+  OrtSessionOptions *session_options;
+  OrtAllocator *allocator;
+} OnnxModel;
+
+static inline void onnx_model_check_status(const OrtApi *api,
+                                           OrtStatus *status) {
+  if (status != NULL) {
+    const char *msg = api->GetErrorMessage(status);
+    fprintf(stderr, "Error: %s\n", msg);
+    api->ReleaseStatus(status);
+    exit(1);
+  }
+}
+
+/// gets input/output names, dims and memory info
+static inline void onnx_model_get_info(OnnxModel *model,
+                                       size_t *num_input_nodes,
+                                       size_t *num_output_nodes,
+                                       char *input_name, char *output_name,
+                                       OrtMemoryInfo *mem_info) {
+  if (!model) {
+    fprintf(stderr, "INVALID OnnxModel!");
+    return;
+  }
+  OrtStatus *status = NULL;
+  status = model->api->SessionGetInputCount(model->session, num_input_nodes);
+  onnx_model_check_status(model->api, status);
+
+  status = model->api->SessionGetInputName(model->session, 0, model->allocator,
+                                           &input_name);
+  onnx_model_check_status(model->api, status);
+
+  status = model->api->SessionGetOutputCount(model->session, num_output_nodes);
+  onnx_model_check_status(model->api, status);
+
+  status = model->api->SessionGetOutputName(model->session, 0, model->allocator,
+                                            &output_name);
+  onnx_model_check_status(model->api, status);
+  model->api->CreateCpuMemoryInfo(OrtArenaAllocator, OrtMemTypeDefault,
+                                  &mem_info);
+  onnx_model_check_status(model->api, status);
+}
+
+static inline OnnxModel *onnx_model_load(const char *model_path) {
+  OnnxModel *model = (OnnxModel *)malloc(sizeof(OnnxModel));
+  if (!model) {
+    fprintf(stderr, "failed to allocate memory for model!\n");
+    return NULL;
+  }
+  memset(model, 0, sizeof(OnnxModel));
+
+  model->api = OrtGetApiBase()->GetApi(ORT_API_VERSION);
+  if (!model->api) {
+    fprintf(stderr, "failed to get onnxruntime api!\n");
+    free(model);
+    return NULL;
+  }
+
+  OrtStatus *status = model->api->CreateEnv(ORT_LOGGING_LEVEL_WARNING,
+                                            "onnx-model", &model->env);
+  if (status != NULL) {
+    onnx_model_check_status(model->api, status);
+    free(model);
+    return NULL;
+  }
+
+  status = model->api->CreateSessionOptions(&model->session_options);
+  onnx_model_check_status(model->api, status);
+
+  status = model->api->SetSessionGraphOptimizationLevel(model->session_options,
+                                                        ORT_ENABLE_BASIC);
+  onnx_model_check_status(model->api, status);
+
+  status = model->api->CreateSession(model->env, model_path,
+                                     model->session_options, &model->session);
+  onnx_model_check_status(model->api, status);
+
+  status = model->api->GetAllocatorWithDefaultOptions(&model->allocator);
+  onnx_model_check_status(model->api, status);
+
+  fprintf(stdout, "model successfully loaded!\n");
+  return model;
+}
+
+static inline void onnx_model_free(OnnxModel *model) {
+  if (model) {
+    if (model->session) {
+      model->api->ReleaseSession(model->session);
+    }
+    if (model->session_options) {
+      model->api->ReleaseSessionOptions(model->session_options);
+    }
+    if (model->env) {
+      model->api->ReleaseEnv(model->env);
+    }
+  }
+  free(model);
+}
+
+
+#ifdef __cplusplus
+ }
+#endif
+
+
+#endif /// onnx utils header
+
